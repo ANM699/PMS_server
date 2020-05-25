4097b0452b9ad660337bf8e6bdfa3bbe8e63d243
ANM699 <kevin_kuo@126.com>
2020-05-25 17:06:41 +0800

    first commit

---
+++ db/models.js
@@ -0,0 +1,27 @@
+//1.连接数据库
+//引入mongoose
+const mongoose = require('mongoose');
+//连接指定数据库
+mongoose.connect('mongodb://localhost:27017/pms', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//获取连接对象
+const conn = mongoose.connection;
+//绑定连接完成的监听
+conn.on('connected', function () {
+  console.log('数据库连接成功');
+});
+
+//2.得到对应特定集合的Model并向外暴露
+//定义Schema
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  memo: { type: String },
+});
+//定义Model
+const UserModel = mongoose.model('user', userSchema);
+//向外暴露Model
+exports.UserModel = UserModel;
---
+++ routes/users.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var router = express.Router();
+
+const md5 = require('blueimp-md5');
+const { UserModel } = require('../db/models');
+const filter = {
+  password: 0,
+  __v: 0,
+}; //查询时过滤指定的属性
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+//用户注册
+router.post('/register', function (req, res) {
+  const { username, password, email } = req.body;
+  UserModel.findOne(
+    {
+      email,
+    },
+    filter,
+    (err, user) => {
+      if (user) {
+        res.send({
+          code: 1,
+          msg: '该邮箱已经被注册',
+        });
+      } else {
+        new UserModel({
+          username,
+          password: md5(password),
+          email,
+        }).save((err, user) => {
+          res.cookie('userid', user._id, {
+            maxAge: 1000 * 60 * 60 * 24,
+          });
+          res.send({
+            code: 0,
+            data: user,
+          });
+        });
+      }
+    }
+  );
+});
+
+//用户登录
+router.post('/login', function (req, res, next) {
+  const { email, password } = req.body;
+  UserModel.findOne(
+    {
+      email,
+      password: md5(password),
+    },
+    filter,
+    (err, user) => {
+      if (user) {
+        res.cookie('userid', user._id, {
+          maxAge: 1000 * 60 * 60 * 24,
+        });
+        // const data = { username, type: user.type, _id: user._id };
+        res.send({
+          code: 0,
+          data: user,
+        });
+      } else {
+        res.send({
+          code: 1,
+          msg: '邮箱或密码错误',
+        });
+      }
+    }
+  );
+});
+
+module.exports = router;
