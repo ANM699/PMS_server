b1b4170f3214e598a1d51230541f8605d632a485
ANM699 <kevin_kuo@126.com>
2020-09-09 00:33:14 +0800

    同步

--- routes/projects.js
+++ routes/projects.js
@@ -1,13 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-const { ProjectModel } = require('../db/models');
+const { ProjectModel } = require("../db/models");
 const filter = {
   __v: 0,
 };
 
 //新增项目
-router.post('/create', function (req, res) {
+router.post("/create", function (req, res) {
   const project = req.body;
   new ProjectModel(project).save((err, project) => {
     res.send({
@@ -18,14 +18,14 @@ router.post('/create', function (req, res) {
 });
 
 //获取项目信息
-router.get('/project', function (req, res) {
+router.get("/project", function (req, res) {
   const projectId = req.cookies.projectId;
   if (!projectId) {
-    return res.send({ code: 1, msg: '未选择项目' });
+    return res.send({ code: 1, msg: "未选择项目" });
   }
   ProjectModel.findOne({ _id: projectId }, filter, (err, project) => {
     if (!project) {
-      res.send({ code: 1, msg: '项目不存在' });
+      res.send({ code: 1, msg: "项目不存在" });
     } else {
       res.send({ code: 0, data: project });
     }
@@ -33,7 +33,7 @@ router.get('/project', function (req, res) {
 });
 
 //获取项目列表
-router.get('/projectList', function (req, res) {
+router.get("/list", function (req, res) {
   ProjectModel.find({}, filter, function (err, projects) {
     res.send({ code: 0, data: projects });
   });
